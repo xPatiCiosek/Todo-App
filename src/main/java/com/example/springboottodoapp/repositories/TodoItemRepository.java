74e53b31c52df325018f1a29df730e401f9af894
patixoa <patrycja_ciosek@hotmail.com>
2024-02-03 13:34:08 +0000

    initial commit

---
+++ src/main/java/com/example/springboottodoapp/repositories/TodoItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboottodoapp.repositories;
+
+import com.example.springboottodoapp.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+}
