be3f442753adbc832190cc929d80f17b3530d965
pati <patrycja_ciosek@hotmail.com>
2024-02-05 12:00:15 +0000

    created a method to add new item

--- src/main/java/com/example/springboottodoapp/controllers/TodoFormController.java
+++ src/main/java/com/example/springboottodoapp/controllers/TodoFormController.java
@@ -1,9 +1,14 @@
 package com.example.springboottodoapp.controllers;
 
+import com.example.springboottodoapp.models.TodoItem;
 import com.example.springboottodoapp.services.TodoItemService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoFormController {
@@ -14,5 +19,15 @@ public class TodoFormController {
     @GetMapping("/create-todo")
     public String showCreateForm(){
         return "new-todo-item";
+    }
+
+    @PostMapping("/todo")
+    public String createTodoItem(@Valid TodoItem todoItem, BindingResult result, Model model){
+        TodoItem item = new TodoItem();
+        item.setDescription(todoItem.getDescription());
+        item.setIsComplete(todoItem.getIsComplete());
+
+        todoItemService.save(todoItem);
+        return "redirect:/";
     }
 }
